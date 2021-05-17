8077be07d4cd1606f6f53913e78e70a79bb9f8f9
Felix Steiner <felix_steiner@bluewin.ch>
2021-05-17 16:09:23 +0200

    fix(*): add lerna config for all packages

--- packages/DynamicPageLoader/handleLinkClicks.window.js
+++
@@ -1,5 +0,0 @@
-// Make import available on window; needed for testing in JSDOM only.
-import handleLinkClicks from './handleLinkClicks.js';
-
-/* global window */
-window.handleLinks = handleLinkClicks;
--- packages/DynamicPageLoader/handlePopState.window.js
+++
@@ -1,5 +0,0 @@
-// Make import available on window; needed for testing in JSDOM only.
-import handlePopState from './handlePopState.js';
-
-/* global window */
-window.handlePopState = handlePopState;
