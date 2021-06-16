35634da9669b790b9cd2a9bfea19586f826344c5
Felix Steiner <felix@joinbox.com>
2021-06-16 11:55:41 +0200

    fix(toc): remove raf

--- packages/TableOfContents/TableOfContentsElement.js
+++ packages/TableOfContents/TableOfContentsElement.js
@@ -51,8 +51,8 @@
     /* global HTMLElement, requestAnimationFrame, document, window */
 
     /**
-     * Table of contents that takes contents (e.g. titles) from the current page depending on the
-     * selector provided.
+     * Overlay that is opened/closed by open/closeoverlay events. Optionally closes on esc or
+     * click outside and always locks background (prevents scrolling).
      */
     class TableOfContents extends HTMLElement {
 
@@ -228,12 +228,14 @@
                 .filter(item => !item.matches(this.templateSelector));
             // Append table of contents after template; template must therefore be placed at the
             // place where content will be inserted
-            requestAnimationFrame(() => {
-                for (const elementToRemove of toRemove) {
-                    template.parentNode.removeChild(elementToRemove);
-                }
-                template.parentNode.appendChild(fragment);
-            });
+            // DO NOT USE RAF! Will not work (will not remove children, as they are not children
+            // of the parent)
+            // requestAnimationFrame(() => {
+            for (const elementToRemove of toRemove) {
+                template.parentNode.removeChild(elementToRemove);
+            }
+            template.parentNode.appendChild(fragment);
+            // });
         }
 
     }
