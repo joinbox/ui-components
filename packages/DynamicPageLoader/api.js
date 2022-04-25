345b0a608a59494c263646c5779f822621c47312
Felix Steiner <felix@joinbox.com>
2022-04-25 11:09:02 +0200

    doc(dpl): new api docs

---
+++ packages/DynamicPageLoader/api.js
@@ -0,0 +1,19 @@
+/*
+- Prevent DPL with
+  - clicked link element and href
+  - page element target
+  - page element source
+
+- Replace element with selector on target and source
+- options for isIdentical (defaults to DPL); isIdentical and canBeIdentical = one file
+
+- Code to execute (for overlay; return false to stop?)
+  - After click
+  - Before load
+  - After load
+  - Before update
+  - After update
+
+- Links to listen (positive) or not listen to (negative selector); after default
+
+*/
