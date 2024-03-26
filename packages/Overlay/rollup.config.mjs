10c7423c79d0345ca925e2ccf132d4a2e44170d1
Felix Steiner <felix_steiner@bluewin.ch>
2024-03-26 10:01:18 +0100

    fix(async-loader): remove unwanted changes from commit

--- packages/Overlay/rollup.config.mjs
+++ packages/Overlay/rollup.config.mjs
@@ -1,13 +1,6 @@
 import createRollupConfig from '../../createRollupConfig.mjs';
-
 const components = [
     'OverlayElement.js',
     'OverlayButtonElement.js',
-    // Expose the original element so that developers are able to extend it
-    'Overlay.js',
 ];
-const rollupConfig = createRollupConfig(components);
-// Export Overlay.js as ES module
-rollupConfig.at(-1).output.format = 'es';
-
-export default rollupConfig;
+export default createRollupConfig(components);
